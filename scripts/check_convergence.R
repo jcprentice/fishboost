92c55410fa5848b2999c44fdf5ee7b99fbe8d7c3
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-02-11 17:27:29 +0000

    Style edit

--- scripts/check_convergence.R
+++ scripts/check_convergence.R
@@ -9,51 +9,53 @@
 check_convergence <- function(dataset = "fb-test") {
     # dataset <- "sim-base-inf"
     # dataset <- "fb-test"
-    
+
     files <- list.files(str_glue("datasets/{dataset}/results"),
                         full.names = TRUE) |>
         str_sort(numeric = TRUE)
-    
-    pes <- map(files, readRDS) |>
+
+    pes <- files |>
+        map(readRDS) |>
         map("parameter_estimates") |>
         rbindlist(idcol = "file")
-    
-    nsamples <- map(files, readRDS) |>
+
+    nsamples <- files |>
+        map(readRDS) |>
         map("params") |>
         map_dbl("nsample")
-    
+
     {
         # Remove group effects
         pes <- pes[!str_starts(parameter, "G_")]
-        
+
         # Add sce, rep, and description
         scens <- files |> str_split_i("/", 4) |> str_split_i("-", 2) |> as.integer()
         reps <- files |> str_split_i("/", 4) |> str_split_i("-", 3) |>
             str_remove_all(".rds") |> as.integer()
         descriptions <- map(files, readRDS) |> map("params") |> map_chr("description") |>
             str_remove_all(", GRM \\w*|, convergence")
-        
+
         pes[, `:=`(scen = scens[file],
                    rep = reps[file],
                    desc = descriptions[file])] |>
             setcolorder(c("scen", "rep", "desc"))
-        
+
         # Rename LPs
         pes[, parameter := parameter |>
                 str_replace_all(c("latent_period" = "LP",
                                   "detection_period" = "DP",
                                   "removal_period" = "RP"))]
-        
+
         pes[, GEV := desc |> str_split_1(", ") |> str_subset("GEV ") |> str_remove("GEV "), .I]
         pes[is.na(GEV), GEV := "SIT"]
         pes[, FE := desc |> str_split_1(", ") |> str_subset("FE  ") |> str_remove("FE "), .I]
         pes[is.na(FE), FE := "SIT"]
-        
+
         # Tidy up
         pes[, `:=`(file = NULL, ci95min = NULL, ci95max = NULL)]
         setcolorder(pes, c("scen", "rep", "GEV"))
         }
-    
+
     pes2 <- pes[, .(desc = first(desc),
                     # GEV = first(GEV),
                     # FE = first(FE),
@@ -67,7 +69,7 @@ check_convergence <- function(dataset = "fb-test") {
                               min_ESS >= 200 & max_GR < 1.1,  "**",
                               min_ESS >= 100 & max_GR < 1.2,  "*",
                               default = "")]
-    
+
     list(summary = pes2,
          worst = pes[order(ESS)[1:20]])
 }
