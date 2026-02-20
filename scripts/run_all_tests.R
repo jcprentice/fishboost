a257abbb5b3b74b0012d16e95f4c89374a967ac6
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-02-20 15:42:44 +0000

    Many improvements

--- scripts/run_all_tests.R
+++ scripts/run_all_tests.R
@@ -4,33 +4,29 @@
     library(purrr)
 
     source("scripts/check_convergence.R")
+    source("scripts/pars_errorbars.R")
     source("scripts/posterior.R")
     source("scripts/plot_ebvs.R")
-    source("scripts/pars_errorbars.R")
     source("plotting/plot_chains.R")
-    source("scripts/correlations.R")
+    source("plotting/plot_correlations.R")
     source("scripts/km_plots.R")
 }
 
 run_all_tests <- function(dataset = "fb-qtest",
                           include_km_plots = FALSE) {
     if (FALSE) {
-        dataset <- "fb-qtest"
+        dataset <- "fb-test"
     }
 
     message(str_glue("Generating all outputs for '{dataset}'"))
 
-    scens <- list.files(str_glue("datasets/{dataset}/results")) |>
-        str_remove_all("scen-|.rds") |>
+    scen_reps <- list.files(str_glue("datasets/{dataset}/results")) |>
         str_sort(numeric = TRUE) |>
-        str_split_i("-", 1) |>
-        map_int(as.integer)
+        str_extract_all("\\d+") |>
+        map(as.integer)
 
-    reps <- list.files(str_glue("datasets/{dataset}/results")) |>
-        str_remove_all("scen-|.rds") |>
-        str_sort(numeric = TRUE) |>
-        str_split_i("-", 2) |>
-        map_int(as.integer)
+    scens <- map_int(scen_reps, 1)
+    reps  <- map_int(scen_reps, 2)
 
     # Check convergence statistics
     out <- check_convergence(dataset)
@@ -39,21 +35,19 @@ run_all_tests <- function(dataset = "fb-qtest",
     # Overall errorbar plot of data
     pars_errorbars(dataset)
 
-    walk2(scens, reps, {
-        possibly(~ plot_chains(dataset, .x, .y))
-        possibly(~ get_posterior(dataset, .x, .y))
-        possibly(~ plot_ebvs(dataset, .x, .y))
-        possibly(~ get_correlations(dataset, .x, .y))
-    })
+    walk2(scens, reps, possibly(~ plot_chains(dataset, .x, .y)))
+    walk2(scens, reps, possibly(~ get_posterior(dataset, .x, .y)))
+    walk2(scens, reps, possibly(~ plot_ebvs(dataset, .x, .y)))
+    walk2(scens, reps, possibly(~ plot_correlations(dataset, .x, .y)))
 
     if (include_km_plots) {
         f <- str_glue("datasets/{dataset}/meta/km_data_ps.rds")
 
-        opts = list(n_plots = 50,
-                    use_means = FALSE)
-        plotopts = c("drop_small_groups",
-                     "drop_donors",
-                     "use_sire_Tinfs")[0]
+        opts <- list(n_plots = 50,
+                     use_means = FALSE)
+        plotopts <- c("drop_small_groups",
+                      "drop_donors",
+                      "use_sire_Tinfs")[0]
 
         km_plots(dataset, scens,
                  simulate_new_data = "bici", opts, plotopts)
@@ -62,9 +56,6 @@ run_all_tests <- function(dataset = "fb-qtest",
 }
 
 if (FALSE) {
-    c("fb-test",
-      "fb-qtest",
-      "fb-qtest-1e5",
-      "fb-qtest-5e5") |>
-        walk(run_all_tests)
+    out <- c("fb-test", "fb-qtest") |>
+        map(run_all_tests)
 }
