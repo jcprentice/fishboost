e039f3285410bba46cb83874c7660c361021c4ab
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-03-03 15:04:12 +0000

    Changed SDcols to use patterns Made subtitle generation more robust

--- pred_accs.R
+++ pred_accs.R
@@ -6,16 +6,15 @@
     library(cowplot)
 }
 
-pred_accs_plot <- function(dataset = "sim-base-inf", name = "scen-5-1", parents_only = TRUE) {
+pred_accs_plot <- function(dataset = "sim-test-inf", name = "scen-1-1", parents_only = TRUE) {
     if (FALSE) {
-        dataset <- "sim-base-inf"; name <- "scen-5-1"; parents_only <- TRUE
+        dataset <- "sim-test-inf"; name <- "scen-1-1"; parents_only <- TRUE
     }
-    
+
     res <- readRDS(str_glue("datasets/{dataset}/results/{name}.rds"))
     params <- res$params
-    
-    x <- res$popn
-    x[, str_subset(names(x), "^(id|sdp|sus|inf|tol|end)", negate = TRUE) := NULL]
+
+    x <- res$popn[, .SD, .SDcols = patterns("^(id|sdp|sus|inf|tol|end)")]
     if (parents_only) x <- x[sdp != "progeny"]
     setnames(x, str_replace_all(names(x),
                                 c("susceptibility" = "sus",
@@ -27,30 +26,29 @@ pred_accs_plot <- function(dataset = "sim-base-inf", name = "scen-5-1", parents_
                                   "BV" = "g", "GV" = "g", "EV" = "e")))
     cols <- c("id", "sdp", "sus_g", "inf_g", "tol_g", "sus_e", "inf_e", "tol_e")
     setcolorder(x, cols, skip_absent = TRUE)
-    
+
     if (any(cols %notin% names(x))) {
         return(NULL)
     }
-    
+
     rf <- str_glue("datasets/{dataset}/data/{name}-out/etc_inf.rds")
     if (!file.exists(rf)) {
         message(str_glue("Warning: missing '{rf}'"))
         return(c(sus = 0, inf = 0, tol = 0))
     }
-    y <- readRDS(rf)$popn
-    y[, str_subset(names(y), "^(id|sdp|sus|inf|tol|end)", negate = TRUE) := NULL]
+    y <- readRDS(rf)$popn[, .SD, .SDcols = patterns("^(id|sdp|sus|inf|tol|end)")]
     if (parents_only) y <- y[sdp != "progeny"]
-    
-    y <- y[, map(.SD, mean), .(id, sdp), .SDcols = str_subset(names(y), "_[ge]$")]
+
+    y <- y[, map(.SD, mean), .(id, sdp), .SDcols = patterns("_[ge]$")]
     setcolorder(y, cols, skip_absent = TRUE)
-    
+
     xy <- merge(melt(x[, .(id, sdp, sus_g, inf_g, tol_g)], id.vars = c("id", "sdp"), value.name = "x"),
                 melt(y[, .(id, sdp, sus_g, inf_g, tol_g)], id.vars = c("id", "sdp"), value.name = "y"))
     # xy <- merge(melt(x, "id", value.name = "x"),
     #             melt(y, "id", value.name = "y"))
     xy[, topx := fifelse(x >= quantile(x, 0.8) & y >= quantile(y, 0.8),
                          "correct", "wrong") |> factor(), variable]
-    
+
     p <- ggplot(xy, aes(x = x, y = y, group = variable, colour = topx)) +
         geom_point() +
         geom_abline(linetype = "dashed", linewidth = 0.5) +
@@ -77,17 +75,20 @@ pred_accs_plot <- function(dataset = "sim-base-inf", name = "scen-5-1", parents_
                                     tol_e = "Environmental Endurance"))) +
         theme_bw() +
         theme(legend.position = "bottom")
-    
+
     title_plt <- ggplot() +
         labs(title = with(params, str_glue("Dataset: '{dataset} / s{scenario}-{replicate}'")),
-             subtitle = str_remove(params$description, ", (coverage|convergence)")) +
-        theme_classic() +
-        theme(plot.title = element_text(size = 22),
+             subtitle = params$description |>
+                 str_split_1(", ") |>
+                 str_subset("coverage|convergence", negate = TRUE) |>
+                 str_collapse_comma()) +
+             theme_classic() +
+             theme(plot.title = element_text(size = 22),
               plot.subtitle = element_text(size = 16))
-    
+
     plt <- plot_grid(title_plt, p,
                      ncol = 1, rel_heights = c(0.2, 1))
-    
+
     ggsave(str_glue("datasets/{dataset}/gfx/pred_accs/{name}-pa{parents}.pdf",
                     parents = if (parents_only) "_parents" else ""),
            plt, width = 15, height = 5)
@@ -99,12 +100,12 @@ pred_accs <- function(dataset = "sim-base-inf", name = "scen-5-1", parents_only 
         dataset <- "sim-base-inf"; name <- "scen-5-1"
         parents_only <- TRUE; method <- "kendall"
     }
-    
+
     x <- readRDS(str_glue("datasets/{dataset}/results/{name}.rds"))$popn
-    x[, str_subset(names(x), "^(id|sdp|sus|inf|tol|end)", negate = TRUE) := NULL]
+    x[, names(.SD) := NULL, .SDcols = !patterns("^(id|sdp|sus|inf|tol|end)")]
     if (parents_only) x <- x[sdp != "progeny"]
     x[, sdp := NULL]
-    
+
     setnames(x, str_replace_all(names(x),
                                 c("susceptibility" = "sus",
                                   "infectivity"    = "inf",
@@ -115,24 +116,23 @@ pred_accs <- function(dataset = "sim-base-inf", name = "scen-5-1", parents_only 
                                   "BV" = "g", "GV" = "g", "EV" = "e")))
     cols <- c("id", "sus_g", "inf_g", "tol_g", "sus_e", "inf_e", "tol_e")
     setcolorder(x, cols, skip_absent = TRUE)
-    
+
     if (any(cols %notin% names(x))) {
         return(c(sus = 0, inf = 0, tol = 0))
     }
-    
+
     rf <- str_glue("datasets/{dataset}/data/{name}-out/etc_inf.rds")
     if (!file.exists(rf)) {
         message(str_glue("Warning: missing '{rf}'"))
         return(c(sus = 0, inf = 0, tol = 0))
     }
-    y <- readRDS(rf)$popn
+    y <- readRDS(rf)$popn[, .SD, .SDcols = patterns("^(id|sdp|sus|inf|tol|end)")]
     if (parents_only) y <- y[sdp != "progeny"]
     y[, sdp := NULL]
-    
-    y[, str_subset(names(y), "^(id|sus|inf|tol|end)", negate = TRUE) := NULL]
-    y <- y[, map(.SD, mean), id, .SDcols = str_subset(names(y), "_[ge]$")]
+
+    y <- y[, map(.SD, mean), id, .SDcols = patterns("_[ge]$")]
     setcolorder(y, cols, skip_absent = TRUE)
-    
+
     if (is.character(method)) {
         if (method %notin% c("pearson", "spearman", "kendall")) {
             method <- "kendall"
@@ -142,15 +142,15 @@ pred_accs <- function(dataset = "sim-base-inf", name = "scen-5-1", parents_only 
           tol = cor.test(x$tol_g, y$tol_g, method = method)$estimate |> unname())
     } else if (is.numeric(method)) {
         p <- method |> max(0.001) |> min(0.999)
-        
+
         x[, `:=`(top_sus = sus_g >= quantile(sus_g, p),
                  top_inf = inf_g >= quantile(inf_g, p),
                  top_tol = tol_g >= quantile(tol_g, p))]
-        
+
         y[, `:=`(top_sus = sus_g >= quantile(sus_g, p),
                  top_inf = inf_g >= quantile(inf_g, p),
                  top_tol = tol_g >= quantile(tol_g, p))]
-        
+
         c(sus = mean(x$top_sus & y$top_sus) / (1 - p),
           inf = mean(x$top_inf & y$top_inf) / (1 - p),
           tol = mean(x$top_tol & y$top_tol) / (1 - p))
@@ -167,7 +167,7 @@ if (FALSE) {
         # out1[, map(.SD, mean)]
     }) |>
         rbindlist(idcol = "scen")
-    
+
     PAs[, .(Scenario = str_c("s", scen),
             Susceptibility = round(100 * sus, 1),
             Infectivity    = round(100 * inf, 1),
