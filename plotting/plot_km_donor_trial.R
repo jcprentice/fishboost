67875e45829833b806b2372407a2d6c7ef8912c7
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-03-06 15:27:09 +0000

    Moved filtering by trial to plotopts

--- plotting/plot_km_donor_trial.R
+++ plotting/plot_km_donor_trial.R
@@ -5,21 +5,22 @@
     library(ggplot2)
 }
 
-plot_km_donor_trial <- function(data_list, trials = 12, plotopts = c()) {
-    
+plot_km_donor_trial <- function(data_list, plotopts = NULL) {
+
     if (FALSE) {
         i <- 2
         data <- copy(km_data[[i]]$data)
         params <- km_data[[i]]$params
         opts <- km_data[[i]]$opts
-        trials <- 12
-        plotopts = c("drop_small_groups")
+        plotopts = c("drop_small_groups", "t1", "t2")[1]
     } else {
         data   <- copy(data_list$data)
         params <- data_list$params
         opts   <- data_list$opts
-    }    
-    
+    }
+
+    # trials <- data[, unique(trial)] |> discard(is.na)
+
     description <- params$description |>
         str_split_1(", ") |>
         str_subset("convergence|coverage|pedigree|GRM", negate = TRUE) |>
@@ -28,51 +29,50 @@ plot_km_donor_trial <- function(data_list, trials = 12, plotopts = c()) {
                           "inf_model 3" = "inf: Don = 0.1*Rec",
                           "inf_model 4" = "inf: Don = r*Rec")) |>
         str_flatten_comma()
-    
+
     # Choose between actual Tinfs and BICI's inferred values for FB data
     if ("use_sire_Tinfs" %in% plotopts && "Tinf_sire" %in% names(data)) {
         fb_Tinfs <- with(params, get_median_Tinfs(dataset, scenario, replicate))
-        
+
         N1 <- data[src == "fb", .N]
         N2 <- nrow(fb_Tinfs)
         data[src == "fb", Tinf2 := fb_Tinfs$Tinf[seq(1 + N2 - N1, N2)]]
     }
-    
+
     # If Tsym is missing but Tdeath is not, then let Tsym = Tdeath
     data[, Tsym2 := fifelse(is.na(Tsym) & !is.na(Tdeath), Tdeath, Tsym)]
-    
+
     # Make a copy of the data and add in rows representing t=0, sort times
     data_t0 <- data[, .(Tinf = c(0, sort(Tinf,  na.last = TRUE)),
                         Tsym = c(0, sort(Tsym2, na.last = TRUE)),
                         RP   = c(0, sort(RP,    na.last = TRUE)),
                         src = src[c(1, 1:.N)]),
                     .(id, donor, trial)]
-    
+
     setorder(data_t0, id, donor, trial)
-    
+
     # Rename src
     data_t0[, src := str_c(src, "_", fifelse(donor == 1, "d", "r"))]
-    
+
     # Create a column to group by, ensure this is wide enough to handle all ids
     # data_t0[, gp := factor(str_c(format(id), "_", donor, "_", trial))]
     data_t0[, gp := .GRP, .(id, donor, trial)]
-    
-    # Split by trial
-    if (trials %in% 1:2) {
-        data_t0 <- data_t0[trial == trials]
-    }
-    
+
+    # Filter by trial
+    if ("t1" %in% plotopts) data_t0 <- data_t0[trial == 1]
+    if ("t2" %in% plotopts) data_t0 <- data_t0[trial == 2]
+
     # Survival curves by id and sire
     data_t0[, survival := seq(1, 0, length.out = .N), gp]
-    
+
     # Melt so we can use facet_wrap
     data_t1 <- melt(data_t0,
                     measure.vars = c("Tinf", "Tsym", "RP"))
-    
+
     if ("show_Tinfs" %notin% plotopts) {
         data_t1 <- data_t1[variable != "Tinf"]
     }
-    
+
     # Drop the really high values
     # data_t1[variable == "Tsym" & value > 200, value := NA]
     # data_t1[variable == "RP"   & value > 160, value := NA]
@@ -80,9 +80,9 @@ plot_km_donor_trial <- function(data_list, trials = 12, plotopts = c()) {
     # data_t1[(trial == 1 & value >= 104) | (trial == 2 & value >= 160), value := NA]
     maxvals <- c(104, 160)
     data_t1[, value := fifelse(value >= maxvals[trial], maxvals[trial], value)]
-    
+
     um <- if (opts$use_means) ", mean" else ", samples"
-    
+
     # Plot
     plt <- ggplot(data_t1) +
         # geom_step
@@ -118,14 +118,14 @@ plot_km_donor_trial <- function(data_list, trials = 12, plotopts = c()) {
                                     RP   = "Proportion of family surviving vs time"),
                        trial = c("1" = "Trial 1", "2" = "Trial 2")))
     plt
-    
+
     # gfx_dir <- str_glue("{dataset}/gfx")
     #
     # ggsave(str_glue("{gfx_dir}/{dataset}-s{scen}-KM_donors-trials.pdf"),
     #        plot = plt, width = 12, height = 8)
     # ggsave(str_glue("{gfx_dir}/{dataset}-s{scen}-KM_grid-trials.png"),
     #        plot = plt, width = 12, height = 8, dpi = 600)
-    # 
+    #
     # plt2 <- plt +
     #     facet_wrap(. ~ variable,
     #                scales = "free_x",
@@ -133,14 +133,14 @@ plot_km_donor_trial <- function(data_list, trials = 12, plotopts = c()) {
     #                    variable = c(Tsym = "Proportion of family uninfected vs time",
     #                                 Tsym = "Proportion of family with no symptoms vs time",
     #                                 RP   = "Proportion of family surviving vs time")))
-    # 
+    #
     # ggsave(str_glue("{gfx_dir}/{dataset}-s{scen}-KM_grid.pdf"),
     #        plot = plt2, width = 12, height = 6)
     # ggsave(str_glue("{gfx_dir}/{dataset}-s{scen}-KM_grid.png"),
     #        plot = plt2, width = 12, height = 6, dpi = 600)
-    # 
+    #
     # plt2
-    
+
     list(plt = plt, data = data_t1)
 }
 
