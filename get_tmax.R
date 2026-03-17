c09a7b078ab4fa7a9a6120101ad4b9338051a48b
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-03-17 15:17:44 +0000

    Fixed whitespace

--- get_tmax.R
+++ get_tmax.R
@@ -3,25 +3,25 @@ get_tmax <- function(popn, params) {
     censor <- params$censor
     time_step <- params$time_step
     sim_new_data <- params$sim_new_data
-    
+
     if (sim_new_data == "no") {
         return(c(t1 = 104, t2 = 160))
     }
-    
+
     x1 <- popn[sdp == "progeny", .(id, trial, Tdeath)]
-    
+
     x1[, Tdeath := fifelse(is.na(Tdeath),
                            max(Tdeath, na.rm = TRUE),
                            Tdeath),
        trial]
-    
+
     x2 <- x1[, quantile(Tdeath, censor, na.rm = TRUE), trial]
     tmax <- setNames(x2$V1, str_c("t", x2$trial))
-    
+
     # Want the nearest time step if time is measured in discrete intervals
     if (time_step > 0) {
         tmax <- ceiling(tmax / time_step) * time_step
     }
-    
+
     tmax
 }
