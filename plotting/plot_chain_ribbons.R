e93b8505094fdce0c6344dac116f51564775b0b7
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-03-17 15:25:17 +0000

    Made more robust

--- plotting/plot_chain_ribbons.R
+++ plotting/plot_chain_ribbons.R
@@ -7,24 +7,31 @@
     library(ggplot2)
 }
 
-plot_chain_ribbons <- function(dataset = "fb-final", scen = 2, use_hpdi = TRUE) {
-    # dataset <- "fb-final"; scen <- 2; use_hpdi <- TRUE
-    
+plot_chain_ribbons <- function(dataset = "fb-test", scen = 1, use_hpdi = TRUE) {
+    if (FALSE) {
+        dataset <- "fb-test"; scen <- 1; use_hpdi <- TRUE
+    }
+
     files <- list.files(str_glue("{dataset}/data/scen-{scen}-1-out"),
                         pattern = "trace",
                         full.names = TRUE) |>
         str_subset("combine", negate = TRUE) |>
         str_sort(numeric = TRUE)
 
+    if (length(files) == 0) {
+        message("No files found!")
+        return(NULL)
+    }
+
     x <- map(files, \(x) fread(x)) |>
         rbindlist(idcol = "chain")
-    
-    x[, str_subset(names(x), "state|Group|L_|Prior|Posterior|Number|log") := NULL]
-    x[, latent_period := as.double(latent_period)]
-    
+
+    x[, str_subset(names(x), "state|Group|^G_|L_|Prior|Posterior|Number|log") := NULL]
+    x[, names(.SD) := map(.SD, as.double)]
+
     xs <- x[, map(.SD, mean), chain] |>
         melt(id.var = "chain", value.name = "mean")
-    
+
     if (use_hpdi) {
         xs_lo <- x[, map(.SD, \(x) hdi(x, credMass = 0.95)[["lower"]]), chain] |>
             melt(id.var = "chain", value.name = "min")
@@ -38,8 +45,8 @@ plot_chain_ribbons <- function(dataset = "fb-final", scen = 2, use_hpdi = TRUE) 
     }
     xs[, `:=`(min = xs_lo$min,
               max = xs_hi$max)]
-    
-    
+
+
     plt <- ggplot(xs, aes(x = chain, colour = variable)) +
         geom_line(aes(y = mean)) +
         geom_point(aes(y = mean)) +
@@ -51,14 +58,14 @@ plot_chain_ribbons <- function(dataset = "fb-final", scen = 2, use_hpdi = TRUE) 
         theme(legend.position = "none") +
         facet_wrap(. ~ variable,
                    scales = "free")
-    
+
     plt
 }
 
-dataset <- "fb-final"; scen <- 2
-
-plt <- plot_chain_ribbons(dataset, scen)
-
-plt_str <- str_glue("{dataset}/gfx/chains/{dataset}-s{scen}-chain_variables.pdf")
-ggsave(plt_str, plt, width = 16, height = 12)
-
+# dataset <- "fb-final"; scen <- 2
+#
+# plt <- plot_chain_ribbons(dataset, scen)
+#
+# plt_str <- str_glue("{dataset}/gfx/chains/{dataset}-s{scen}-chain_variables.pdf")
+# ggsave(plt_str, plt, width = 16, height = 12)
+#
