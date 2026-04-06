b125d27359ec5249b49d8a0d66b434827c0de6ef
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-04-06 15:02:52 +0100

    Update to take advantage of all available cores

--- run_bici_sim.R
+++ run_bici_sim.R
@@ -7,44 +7,46 @@ run_bici_sim <- function(dataset = "fb-test",
                          name = "scen-1-1",
                          bici_cmd = "post-sim",
                          nreps = 0) {
-    
+
     # dataset <- "sim-base-inf"; name <- "scen-5-1"; bici_cmd <- "post-sim"; nreps <- 0
-    
+
     bici_file <- str_glue("datasets/{dataset}/data/{name}.bici")
-    
+
     if (!file.exists(bici_file)) {
         message(str_glue("Missing BICI file for '{name}'"))
         return(NULL)
     }
-    
-    # Open config file and get no. of reps (cf_reps)
+
+    # Open config file and get no. of reps (reps_in_cf)
     lines <- readLines(bici_file)
     line_n <- str_which(lines, switch(bici_cmd, "sim" = "^simulation.*number", "^post-sim.*number"))
     parts <- str_split_1(lines[[line_n]], " ")
     part_n <- str_which(parts, "number")
-    cf_reps <- str_split_i(parts[[part_n]], "=", 2) |> as.numeric()
-    
-    if (nreps %notin% c(NA, 0, cf_reps)) {
+    reps_in_cf <- str_split_i(parts[[part_n]], "=", 2) |> as.numeric()
+
+    if (nreps %notin% c(NA, 0, reps_in_cf)) {
         message(str_glue("modifying '{name}.bici' to change reps"))
         parts[[part_n]] <- str_glue("number={nreps}")
         line <- str_flatten(parts, " ")
         lines[[line_n]] <- line
         writeLines(lines, bici_file)
     }
-    
+
     cmd <- str_glue(
         "mpirun -n {cores} --output :raw --oversubscribe",
         "../BICI/bici-{platform}", bici_file, bici_cmd,
         .sep = " ",
         platform = Sys.info()[["sysname"]],
-        cores = 2 #primes::gcd(parallel::detectCores(), cf_reps)
+        cores = primes::gcd(parallel::detectCores(), reps_in_cf)
     )
     message(cmd)
-    
+
     out <- system(cmd)
     if (out != 0) return(NULL)
-    
-    flatten_bici_states(dataset, name, bici_cmd)
+
+    flatten_bici_states(list(dataset = dataset,
+                             name = name,
+                             bici_cmd = bici_cmd))
 }
 
 # etc <- run_bici_sim(dataset = "fb-test", "scen-1-1", "post-sim", 50)
