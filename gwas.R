8040e84b0f71b4ea60e07318bcfc995486229cff
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-04-27 17:35:15 +0100

    Hugely shrunk this down

--- gwas.R
+++ gwas.R
@@ -6,148 +6,110 @@
 
 # Load and sample EBV files ----
 
-dataset <- "fb-test"; scen <- 2
-
-f <- str_glue("datasets/{dataset}/data/scen-{scen}-1-out/etc_inf.rds")
-ebvs <- readRDS(f)$popn[, .(state, id, sdp, sus_g, inf_g, tol_g, sus_e, inf_e, tol_e)]
-ebvs[, `:=`(sus_pt = sus_g + sus_e,
-            inf_pt = inf_g + inf_e,
-            tol_pt = tol_g + tol_e)]
-
-trials <- readRDS(f)$popn[sdp == "progeny", unique(trial)]
-if (length(trials) == 1) {
-    fb12 <- readRDS("fb_data/fb_12_rpw.rds")
-    
-    sires <- intersect(fb12[sdp == "sire", id], fb12[sdp == "progeny" & trial %in% trials, sire])
-    all_sires <- fb12[sdp == "sire", id]
-    dams <- intersect(fb12[sdp == "dam", id], fb12[sdp == "progeny" & trial %in% trials, dam])
-    all_dams <- fb12[sdp == "dam", id]
-    progeny <- fb12[sdp == "progeny" & trial %in% trials, id]
-    all_progeny <- fb12[sdp == "progeny", id]
-    
-    nstates <- last(ebvs$state)
-    
-    ebvs[sdp == "sire",     id := rep(sires,   nstates)]
-    ebvs[sdp == "dam",      id := rep(dams,    nstates)]
-    ebvs[sdp == "progeny",  id := rep(progeny, nstates)]
-    
-    # Missing rows
-    mr <- rbind(
-        data.table(sdp = "sire", id = setdiff(all_sires, sires)),
-        data.table(sdp = "dam",  id = setdiff(all_dams, dams)),
-        data.table(sdp = "progeny", id = setdiff(all_progeny, progeny))
-    )
-    mr2 <- mr[rep(seq(.N), nstates)]
-    mr2[, state := rep(seq_len(nstates), each = nrow(mr))]
-    ebvs2 <- rbind(ebvs, mr2, fill = TRUE)
-    
-    setorder(ebvs2, state, id)
-    ebvs <- ebvs2
-    rm(ebvs2, mr, mr2)
-}
+gwas <- function(dataset = "fb-test", scen = 1, rep = 1) {
+    if (FALSE) {
+        dataset <- "fb-test"
+        scen <- 1
+        rep <- 1
+    }
 
 
-# Maps ----
-source("get_fb_map.R")
+    f <- str_glue("datasets/{dataset}/data/scen-{scen}-{rep}-out/etc_inf.rds")
+    ebvs <- readRDS(f)$popn[, .(state, id, sdp, sus_g, inf_g, tol_g, sus_e, inf_e, tol_e)]
+    ebvs[, `:=`(sus_p = sus_g + sus_e,
+                inf_p = inf_g + inf_e,
+                tol_p = tol_g + tol_e)]
 
-# Load the PrunedPed file
-pp <- fread("Hinv_drop71/sequence/PrunedPed.txt") |>
-    setnames(c("id1", "sire1", "dam1", "fb_id", "fb_sire", "fb_dam"))
+    trials <- readRDS(f)$popn[sdp == "progeny", unique(trial)]
+    if (length(trials) == 1) {
+        fb12 <- readRDS("fb_data/fb_12_rpw.rds")
 
-# get the map between PP and FB masterfile
-maps <- get_fb_map()
+        sires <- intersect(fb12[sdp == "sire", id], fb12[sdp == "progeny" & trial %in% trials, sire])
+        all_sires <- fb12[sdp == "sire", id]
+        dams <- intersect(fb12[sdp == "dam", id], fb12[sdp == "progeny" & trial %in% trials, dam])
+        all_dams <- fb12[sdp == "dam", id]
+        progeny <- fb12[sdp == "progeny" & trial %in% trials, id]
+        all_progeny <- fb12[sdp == "progeny", id]
 
-dt <- maps$fb_data
-dt[, `:=`(mf = pp[maps$pp_to_fb, fb_id],
-          gt = pp[maps$pp_to_fb, id1],
-          pp_to_fb = maps$pp_to_fb,
-          fb_to_pp = maps$fb_to_pp)]
+        nstates <- last(ebvs$state)
+
+        ebvs[sdp == "sire",    id := rep(sires,   nstates)]
+        ebvs[sdp == "dam",     id := rep(dams,    nstates)]
+        ebvs[sdp == "progeny", id := rep(progeny, nstates)]
+
+        # Missing rows
+        mr <- rbind(
+            data.table(sdp = "sire",    id = setdiff(all_sires,   sires)),
+            data.table(sdp = "dam",     id = setdiff(all_dams,    dams)),
+            data.table(sdp = "progeny", id = setdiff(all_progeny, progeny))
+        )
+        mr2 <- mr[rep(seq(.N), nstates)]
+        mr2[, state := rep(seq_len(nstates), each = nrow(mr))]
+        ebvs2 <- rbind(ebvs, mr2, fill = TRUE)
+
+        setorder(ebvs2, state, id)
+        ebvs <- ebvs2
+        rm(ebvs2, mr, mr2)
+    }
 
 
-# awk '{print $1}' PrunedGenotype.txt > ids.txt
-ids <- fread("../gwas/ids.txt") |> setnames("id")
-ids[, name := str_c("id", id)]
+    # Maps ----
+    source("get_fb_map.R")
 
-# rows <- data.table(snp = str_c("snp", seq(0,1274747)))
+    # Load the PrunedPed file
+    pp <- fread("Hinv_drop71/sequence/PrunedPed.txt") |>
+        setnames(c("id1", "sire1", "dam1", "fb_id", "fb_sire", "fb_dam"))
 
-# get the new map and check it against dt
-new_map <- match(ids$id, dt$gt)
-dt2 <- dt[new_map]
+    # get the map between PP and FB masterfile
+    maps <- get_fb_map()
 
-# Create EBV siles
+    dt <- maps$fb_data
+    dt[, `:=`(mf = pp[maps$pp_to_fb, fb_id],
+              gt = pp[maps$pp_to_fb, id1],
+              pp_to_fb = maps$pp_to_fb,
+              fb_to_pp = maps$fb_to_pp)]
 
-{
-    gts_sus <- dcast(ebvs[, .(state, id, sus_g)], id ~ state, value.var = "sus_g")[new_map]
-    gts_inf <- dcast(ebvs[, .(state, id, inf_g)], id ~ state, value.var = "inf_g")[new_map]
-    gts_tol <- dcast(ebvs[, .(state, id, tol_g)], id ~ state, value.var = "tol_g")[new_map]
-    evs_sus <- dcast(ebvs[, .(state, id, sus_e)], id ~ state, value.var = "sus_e")[new_map]
-    evs_inf <- dcast(ebvs[, .(state, id, inf_e)], id ~ state, value.var = "inf_e")[new_map]
-    evs_tol <- dcast(ebvs[, .(state, id, tol_e)], id ~ state, value.var = "tol_e")[new_map]
-    pts_sus <- dcast(ebvs[, .(state, id, sus_pt)], id ~ state, value.var = "sus_pt")[new_map]
-    pts_inf <- dcast(ebvs[, .(state, id, inf_pt)], id ~ state, value.var = "inf_pt")[new_map]
-    pts_tol <- dcast(ebvs[, .(state, id, tol_pt)], id ~ state, value.var = "tol_pt")[new_map]
-}
 
-{
-    gts_sus[, id := ids$name]
-    gts_inf[, id := ids$name]
-    gts_tol[, id := ids$name]
-    evs_sus[, id := ids$name]
-    evs_inf[, id := ids$name]
-    evs_tol[, id := ids$name]
-    pts_sus[, id := ids$name]
-    pts_inf[, id := ids$name]
-    pts_tol[, id := ids$name]
-}
+    # awk '{print $1}' PrunedGenotype.txt > ids.txt
+    ids <- fread("../gwas/ids.txt") |> setnames("id")
+    ids[, name := str_c("id", id)]
 
-{
-    cols <- c("id", str_c("sample", seq_len(ncol(gts_sus) - 1L)))
-    setnames(gts_sus, cols)
-    setnames(gts_inf, cols)
-    setnames(gts_tol, cols)
-    setnames(evs_sus, cols)
-    setnames(evs_inf, cols)
-    setnames(evs_tol, cols)
-    setnames(pts_sus, cols)
-    setnames(pts_inf, cols)
-    setnames(pts_tol, cols)
-}
+    # rows <- data.table(snp = str_c("snp", seq(0,1274747)))
 
-# Add in mean and SD
-{
-    gts_sus[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    gts_inf[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    gts_tol[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    evs_sus[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    evs_inf[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    evs_tol[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    pts_sus[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    pts_inf[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-    pts_tol[, `:=`(mean = apply(.SD, 1, mean), sd = apply(.SD, 1, sd)), .SDcols = -1]
-}
+    # get the new map and check it against dt
+    new_map <- match(ids$id, dt$gt)
+    dt2 <- dt[new_map]
 
-# Reorder
-{
-    setcolorder(gts_sus, c("id", "mean", "sd"))
-    setcolorder(gts_inf, c("id", "mean", "sd"))
-    setcolorder(gts_tol, c("id", "mean", "sd"))
-    setcolorder(evs_sus, c("id", "mean", "sd"))
-    setcolorder(evs_inf, c("id", "mean", "sd"))
-    setcolorder(evs_tol, c("id", "mean", "sd"))
-    setcolorder(pts_sus, c("id", "mean", "sd"))
-    setcolorder(pts_inf, c("id", "mean", "sd"))
-    setcolorder(pts_tol, c("id", "mean", "sd"))
-}
 
-# Write to files
-{
-    fwrite(gts_sus, file = "../gwas/fb_gts_sus.csv")
-    fwrite(gts_inf, file = "../gwas/fb_gts_inf.csv")
-    fwrite(gts_tol, file = "../gwas/fb_gts_tol.csv")
-    fwrite(evs_sus, file = "../gwas/fb_evs_sus.csv")
-    fwrite(evs_inf, file = "../gwas/fb_evs_inf.csv")
-    fwrite(evs_tol, file = "../gwas/fb_evs_tol.csv")
-    fwrite(pts_sus, file = "../gwas/fb_pts_sus.csv")
-    fwrite(pts_inf, file = "../gwas/fb_pts_inf.csv")
-    fwrite(pts_tol, file = "../gwas/fb_pts_tol.csv")
+    # Create EBV files
+
+    BVs <- expand.grid(c("g", "e", "p"), c("sus", "inf", "tol")) |>
+        rev() |> apply(1, str_flatten, "_")
+
+    out <- map(BVs, \(bv) {
+        ebv <- ebvs[, mget(c("state", "id", bv))]
+        x <- dcast(ebv, id ~ state, value.var = bv)[new_map]
+        setnames(x, modify_if(names(x), ~ .x != "id", ~ str_c("sample", .x)))
+        x[, id := ids$name]
+
+        x[, `:=`(mean = apply(.SD, 1, mean),
+                 sd = apply(.SD, 1, sd)),
+          .SDcols = -1] |>
+            setcolorder(c("id", "mean", "sd"))
+    }) |>
+        setNames(BVs)
+
+    gwas_dir <- str_glue("../gwas/{dataset}-s{scen}-{rep}")
+    if (!dir.exists(gwas_dir)) {
+        message("- mkdir ", gwas_dir)
+        dir.create(gwas_dir)
+    }
+
+    walk(BVs, \(bv) {
+        f <- str_glue("{gwas_dir}/{x}.csv",
+                      x = str_replace_all(bv, c("_g$" = "_gen",
+                                                "_e$" = "_env",
+                                                "_p$" = "_pt")))
+        fwrite(out[[bv]], f)
+    })
 }
