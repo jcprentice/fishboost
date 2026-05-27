18ee27c67bcf4b0af866ffa3b4c7a341adefd74c
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-05-27 15:19:21 +0100

    Fixed get_median_Tinfs() to just produce a simple vector

---
+++ figures/km_plot.R
@@ -0,0 +1,51 @@
+library(cowplot)
+
+# These were generated using plot_km_family_trial()
+data_t3 <- readRDS("figures/data/fig_km_2.rds")
+data_t3 <- readRDS("figures/data/fig_km_8.rds")
+
+scm <- rowwiseDT(
+    breaks=, labels=,              values=,
+    "fb_d",  "Data Seeder",        "#33A02C",
+    "sim_d", "Simulation Seeder",  "#B2DF8A",
+    "fb_r",  "Data Contact",       "#1F78B4",
+    "sim_r", "Simulation Contact", "#A6CEE3"
+)
+
+p1 <- ggplot(data_t3) +
+    geom_line(aes(x = time, y = survival,
+                  group = gp, colour = str,
+                  linewidth = src, linetype = src)) +
+    scale_fill_manual(breaks = scm$breaks,
+                      labels = scm$labels,
+                      values = scm$values) +
+    scale_colour_manual(breaks = scm$breaks,
+                        labels = scm$labels,
+                        values = scm$values) +
+    scale_linewidth_manual(breaks = c("fb", "sim"),
+                           values = c(0.5, 0.2),
+                           guide = "none") +
+    scale_linetype_manual(breaks = c("fb", "sim"),
+                          values = c("solid", "solid"),
+                          guide = "none") +
+    lims(y = c(0, 1)) +
+    # coord_cartesian(expand = FALSE) +
+    labs(colour = "Source",
+         #linewidth = "Source",
+         x = "Time (days)",
+         y = "Proportion") +
+        facet_grid(cols = vars(variable),
+                   rows = vars(trial),
+                   scales = "free_x",
+                   labeller = labeller(
+                       variable = c(Tsign = "(a) Time to visual signs",
+                                    RP    = "(b) Time from visual signs to death"),
+                       trial = c("1" = "Trial 1",
+                                 "2" = "Trial 2"))) +
+    theme_bw() +
+    theme(legend.position = "bottom",
+          strip.background = element_blank())
+p1
+
+ggsave("gfx/KM_trial_1.png", p1,
+       width = 17.8, height = 10, units = "cm")
--- get_Tinfs.R
+++ get_Tinfs.R
@@ -5,7 +5,9 @@
 }
 
 get_Tinfs <- function(dataset = "fb-test", scen = 1, rep = 1, nsamples = 0) {
-    # dataset <- "fb-test"; scen <- 1; rep <- 1; nsamples <- 0;
+    if (FALSE) {
+        dataset <- "fb-test"; scen <- 1; rep <- 1; nsamples <- 0;
+    }
 
     f <- str_glue("datasets/{dataset}/data/scen-{scen}-{rep}-out/summary_inf.rds")
     popn <- readRDS(f)$popn
@@ -17,16 +19,22 @@ get_Tinfs <- function(dataset = "fb-test", scen = 1, rep = 1, nsamples = 0) {
             sort()
     }
 
-    popn[state %in% states, .(state, id, Tinf)] |>
+    popn[sdp == "progeny" & state %in% states, .(state, id, Tinf)] |>
         dcast(id ~ state, value.var = "Tinf")
 }
 
-get_median_Tinfs <- function(dataset = "fb-test", scen = 1, rep = 1) {
-    # dataset <- "fb-test"; scen <- 1; rep <- 1
+get_median_Tinfs <- function(params) {
+    if (FALSE) {
+        dataset <- "fb-test"; scen <- 1; rep <- 1
+    } else {
+        dataset <- params$dataset
+        scen <- params$scen
+        rep <- params$replicate
+    }
 
     f <- str_glue("datasets/{dataset}/data/scen-{scen}-{rep}-out/summary_inf.rds")
     popn <- readRDS(f)$popn
 
     popn[is.na(Tinf), Tinf := Inf]
-    popn[, .(Tinf = median(Tinf)), id]
+    popn[sdp == "progeny", .(Tinf = median(Tinf)), id][, Tinf]
 }
