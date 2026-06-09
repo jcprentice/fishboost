0127befffe67b8044db4d2e0a336ad0c0389fc03
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-06-09 15:05:42 +0100

    Print Parameter Estimates in table format

---
+++ print_PEs.R
@@ -0,0 +1,49 @@
+{
+    library(data.table)
+    library(stringr)
+    library(purrr)
+    library(DT)
+
+    source("rename_pars.R")
+}
+
+print_PEs <- function(dataset = "fb-test", scen = 2) {
+    if (FALSE) {
+        dataset <- "fb-test"
+        scen <- 2
+    }
+    x <- readRDS(str_glue("datasets/{dataset}/results/scen-{scen}-1.rds"))
+
+    pes <- x$parameter_estimates[!str_starts(parameter, "G"),
+                                 .(parameter, mean, median, hdi95min, hdi95max)]
+    p2 <- merge(pes, x$params$priors[, .(parameter, type, val1, val2)], all.x = TRUE)
+    setcolorder(p2, c("type", "val1", "val2"), after = "parameter")
+
+    p2 <- p2[match(pes$parameter, p2$parameter)]
+
+    p2[, Parameter := pretty_names(parameter)]
+
+    p2[, Prior := str_glue("{x}({v1}, {v2})",
+                           x = fcase(str_detect(parameter, "cov"), "Half Normal",
+                                     str_detect(parameter, "^r_"), "LKJ",
+                                     type == "inverse", "Jeffreys",
+                                     type == "uniform", "Uniform"),
+                           v1 = val1, v2 = val2)]
+
+    p2[, d := fcase(
+        str_detect(parameter, "_[GE]_"), 2,
+        str_detect(parameter, "period"), 1,
+        str_detect(parameter, "weight"), 1,
+        default = 1
+    )]
+
+    p2[, `:=`(
+        Mean   = sprintf(str_glue("%5 .{d}f", d = d), mean),
+        Median = sprintf(str_glue("%5 .{d}f", d = d), median)
+    )]
+
+    p2[, hdi95 := sprintf(str_glue("(%5 .{d}f, %5 .{d}f)", d = d),
+                          hdi95min, hdi95max), .I]
+
+    p2[, .(Parameter, Prior, Mean, Median, hdi95)]
+}
