824a71eb50b1167e942bda725f11f5c0ab9fc3fa
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-06-19 13:18:30 +0100

    Replaced expand_limits

--- scripts/ind_traces.R
+++ scripts/ind_traces.R
@@ -9,32 +9,32 @@
 ind_traces <- function(dataset = "fb-fes3", scen = 1) {
     # dataset <- "fb-final-old"; scen <- 1
     message(str_glue("Getting values for {dataset} / s{scen}"))
-    
+
     # name of state file
     sfiles <- list.files(str_glue("{dataset}/data/scen-{scen}-1-out"),
                          pattern = "state",
                          recursive = TRUE,
                          full.names = TRUE) |>
         str_sort(numeric = TRUE)
-    
+
     event_times <- c("tE", "tI", "tD", "tR")
-    
+
     map(seq_along(sfiles), \(i) {
         sfile <- sfiles[[i]]
         # search for line numbers featuring the word "Individual"
         lines <- str_which(readLines(sfile), "Individual")
-        
+
         # fread only lines in between, fill columns
         x <- fread(sfile, skip = lines[[1]], nrows = diff(lines) - 1, fill = TRUE)
         setnames(x, c("id", event_times))
         x[, id := NULL]
-        
+
         # Convert to numeric, with "no" as NA
         walk(event_times, \(e) {
             x[get(e) == "no", (e) := ""]
             x[, (e) := as.numeric(get(e))]
         })
-        
+
         cbind(
             x[, map(.SD, max,  na.rm = TRUE)] |> setnames(str_c(event_times, "_max")),
             x[, map(.SD, mean, na.rm = TRUE)] |> setnames(str_c(event_times, "_mean"))
@@ -69,7 +69,7 @@ plt_tD <- map(seq_len(nScens), possibly(\(i) {
     ggplot() +
         geom_line(data = maxes, aes(x = id, y = tD_max), colour = "red") +
         geom_line(data = means, aes(x = id, y = tD_mean), colour = "blue") +
-        expand_limits(y = 0) +
+        scale_y_continuous(limits = ~ range(.x, 0)) +
         labs(title = title_str)
 }))
 
