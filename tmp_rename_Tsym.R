4c9867c2389e9b2e83b1392716e5f1fd8423143e
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-06-23 15:32:22 +0100

    Rename Tsym to Tsign in applicable files

--- tmp_rename_Tsym.R
+++ tmp_rename_Tsym.R
@@ -9,31 +9,59 @@ files <- list.files("datasets", ".rds",
     str_subset("r0_selection|meta", negate = TRUE) |>
     str_sort(numeric = TRUE)
 
+modded_res_files <- c()
 
 # Results files
 res_files <- str_subset(files, "results")
 walk(res_files, \(f) {
     x <- readRDS(f)
-    x$params$timings <- x$params$timing |>
-        str_replace_all("Tsym", "Tsign")
-    x$params$pass_events <- x$params$pass_events |>
-        str_replace_all("Tsym", "Tsign")
-    x$popn |>
-        setnames("Tsym", "Tsign", skip_absent = TRUE)
+    touched <- FALSE
+
+    if ("Tsym" %in% x$params$timings) {
+        touched <- TRUE
+        message(f, ": timings")
+        x$params$timings <- x$params$timing |>
+            str_replace_all("Tsym", "Tsign")
+    }
+
+    if ("Tsym" %in% x$params$pass_events) {
+        touched <- TRUE
+        message(f, ": pass_events")
+        x$params$pass_events <- x$params$pass_events |>
+            str_replace_all("Tsym", "Tsign")
+    }
+
+    if ("Tsym" %in% names(x$popn)) {
+        touched <- TRUE
+        message(f, ": popn")
+        x$popn[, Tsign := Tsym]
+        x$popn[, Tsym := NULL]
+    }
+
+    if (touched) {
+        modded_res_files <<- c(modded_res_files, f)
+        saveRDS(x, f)
+    }
+
 }, .progress = TRUE)
 
 
 # Summary files
+modded_sum_files <- c()
 sum_files <- str_subset(files, "summary")
 walk(sum_files, \(f) {
     x <- readRDS(f)
     touched <- FALSE
+
     if ("transitions" %in% names(x)) {
+        touched <- TRUE
         x$transitions |>
             setnames("Tsym", "Tsign", skip_absent = TRUE)
-        touced <- TRUE
     }
 
-    if (touched) saveRDS(x, file = f)
+    if (touched) {
+        modded_sum_files <<- c(modded_sum_files, f)
+        saveRDS(x, file = f)
+    }
 }, .progress = TRUE)
 
