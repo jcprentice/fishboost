28df0dc2f1d54dddaee877d4cb421e3e0e9b15bc
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-06-23 15:26:49 +0100

    Added support for showing penotypes and heritability

--- print_PEs.R
+++ print_PEs.R
@@ -22,6 +22,10 @@ print_PEs <- function(dataset = "fb-test", scen = 2) {
     p2 <- p2[match(pes$parameter, p2$parameter)]
 
     p2[, Parameter := pretty_names(parameter)]
+    p2[, Parameter := str_replace_all(Parameter,
+                                      c(" Gen" = "G",
+                                        " Env" = "E",
+                                        " PT"  = "P"))]
 
     p2[, Prior := str_glue("{x}({v1}, {v2})",
                            x = fcase(str_detect(parameter, "cov"), "Half Normal",
@@ -29,21 +33,31 @@ print_PEs <- function(dataset = "fb-test", scen = 2) {
                                      type == "inverse", "Jeffreys",
                                      type == "uniform", "Uniform"),
                            v1 = val1, v2 = val2)]
+    p2[str_detect(Prior, "LKJ"), Prior := "LKJ(1.2)"]
 
     p2[, d := fcase(
-        str_detect(parameter, "_[GE]_"), 2,
+        str_detect(parameter, "_[GEP]_|h2_"), 2,
         str_detect(parameter, "period"), 1,
         str_detect(parameter, "weight"), 1,
         default = 1
     )]
 
     p2[, `:=`(
-        Mean   = sprintf(str_glue("%5 .{d}f", d = d), mean),
-        Median = sprintf(str_glue("%5 .{d}f", d = d), median)
+        Mean   = round(mean, 2),  # sprintf(str_glue("%5 .{d}f", d = d), mean),
+        Median = round(median, 2) # sprintf(str_glue("%5 .{d}f", d = d), median)
     )]
 
-    p2[, hdi95 := sprintf(str_glue("(%5 .{d}f, %5 .{d}f)", d = d),
-                          hdi95min, hdi95max), .I]
+    # p2[, `95% HDI` := sprintf(str_glue("(%5 .{d}f, %5 .{d}f)", d = d),
+    #                       hdi95min, hdi95max), .I]
+    p2[, `95% HDI` := str_glue("({x}, {y})",
+                               x = round(hdi95min, 2),
+                               y = round(hdi95max, 2)), .I]
 
-    p2[, .(Parameter, Prior, Mean, Median, hdi95)]
+    p2[str_detect(parameter, "_P_|h2"), `:=`(Prior = "", `95% HDI` = "")]
+
+    p3 <- p2[, .(Parameter, Prior, Mean, Median, `95% HDI`)]
+    fwrite(p3, file = "parameter_estimates.tsv", sep = "\t")
+    p3
 }
+
+print_PEs()
