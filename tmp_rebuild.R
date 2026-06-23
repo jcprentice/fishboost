d613d2b65c1ffcd71e87cbabe9f0fbfdf885bc1f
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-06-23 15:31:35 +0100

    Added function to rebuild posteriors with phenotypes and heritability

---
+++ tmp_rebuild.R
@@ -0,0 +1,19 @@
+{
+    library(purrr)
+    library(stringr)
+    source("rebuild_posteriors.R")
+}
+
+datasets <- c("fb-test",
+              "sim-test-inf1",
+              "sim-test-inf2")
+
+walk(datasets, \(ds) {
+    names <- list.files(str_glue("datasets/{ds}/results")) |>
+        str_remove_all(".rds") |>
+        str_sort(numeric = TRUE)
+
+    walk(names, \(name) {
+        rebuild_bici_posteriors(ds, name)
+    })
+})
