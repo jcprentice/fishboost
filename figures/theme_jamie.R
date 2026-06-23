8f7c2011bfd5a775ccfb0608206edc2abedbfe17
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-06-23 15:30:51 +0100

    Simplified slightly

--- figures/theme_jamie.R
+++ figures/theme_jamie.R
@@ -1,8 +1,6 @@
 library(ggplot2)
 library(ggtext)
 
-l2p <- 1 / ggplot2::.pt
-
 theme_jamie <- function() {
     theme_classic() +
     theme(legend.position = "none",
@@ -11,6 +9,6 @@ theme_jamie <- function() {
           # panel.background = element_blank(),
           # panel.grid.major = element_blank(),
           # panel.grid.minor = element_blank(),
-          axis.line = element_line(linewidth = 1 * l2p),
-          axis.ticks = element_line(linewidth = 0.5 * l2p))
+          axis.line = element_line(linewidth = 1 / .pt),
+          axis.ticks = element_line(linewidth = 0.5 / .pt))
 }
