eeb8f5fc63df1fc5f67ce6a7906332f43a0d7566
Jamie Prentice <jamie.c.prentice@gmail.com>
2026-07-02 13:49:47 +0100

    Updated TP names, fixed a bug, simplified considerably

--- get_subgroup_vals.R
+++ get_subgroup_vals.R
@@ -5,42 +5,36 @@
 }
 
 get_subgroup_vals <- function(dataset = "fb-test", scen = 1) {
-    # dataset <- "fb-test"; scen <- 1
+    if (FALSE) {
+        dataset <- "fb-test"; scen <- 7
+    }
 
     f <- str_glue("datasets/{dataset}/results/scen-{scen}-1.rds")
     popn <- readRDS(f)$popn
     pe <- readRDS(f)$parameter_estimates
 
-    # pe[str_starts(parameter, "beta|LP|DP"),
-    #    .(par = parameter,
-    #      mean = round(mean, 3))] |>
-    #     with(setNames(mean, par)) |>
-    #     print()
-    #
-    # message("")
-
-    x <- (popn[, .(id, sdp, trial, donor)]
-          [sdp == "progeny", .N, .(trial, donor)]
-          [, donor := fifelse(donor == 1, "Don", "Rec")])
+    x <- popn[sdp == "progeny",
+              .(id, trial, donor = fifelse(donor == 1, "Don", "Rec"))] |>
+        _[, .N, .(trial, donor)]
 
     x[, `:=`(p = N / sum(N), N = NULL)] # should be 0.5, 0.5
     x[, `:=`(
         beta = rep(pe[str_starts(parameter, "beta"), mean], each = 2),
-        LP = pe[str_starts(parameter, "latent"), mean],
-        DP = pe[str_starts(parameter, "detection"), mean],
-        RP = pe[str_starts(parameter, "removal"), mean]
+        LP   = pe[str_starts(parameter, "LP"), mean],
+        DP   = pe[str_starts(parameter, "DP"), mean],
+        RP   = pe[str_starts(parameter, "RP"), mean]
     )]
 
+    # Summary of all subgroup values
     x[, .(trial, donor, beta = round(beta, 2),
           LP = round(LP, 2), DP = round(DP, 2), RP = round(RP, 2))] |>
         print()
 
-    message("")
-
-    means <- x[, map2(p, .SD, ~ sum(.x * .y)) |> map(round, 2) |> setNames(names(.SD)),
-      .SDcols = c("beta", "LP", "DP", "RP")]
-
-    means[1] |> as.list() |> unlist() |> setNames(names(means))
+    #
+    x[, map(.SD, ~ sum(p * .x)) |> map(round, 2), .SDcols = -(1:3)] |>
+        unlist()
 }
 
 get_subgroup_vals("fb-test", 1)
+get_subgroup_vals("fb-test", 2)
+get_subgroup_vals("fb-test", 7)
